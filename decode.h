17ddb780a890b96989563693d511f0bb6bbfca55
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2024-12-24 19:11:21 -0300

    Update decode.h
    Atualização conforme o que foi feito em id_main.c; edições a ser feitas nos próximos dias.

--- decode.h
+++ decode.h
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <errno.h>
+#include "io_pbm.h" //código a ser feito ainda
 #include "id_main.c" //acho que algumas coisas desse vão ter que vir em h 
-#include "ean8.h" //código a ser feito ainda
 #define WIDTH 100 //colocar esse números no arquivo h
 #define HEIGHT 100
 
@@ -35,12 +35,23 @@ const int right_digit_patterns[10][7] = {
    {1, 0, 0, 1, 0, 0, 0}
    {1, 1, 1, 0, 1, 0, 0}
 };
-//funções pra deixar em arquivo h
-void carrega_pbm(const char *filename, int image[HEIGHT][WIDTH]){
-    //carregamento de imagem aqui
+
+void decode_ean8(int bin_representa[], int decode_d[]){
+    //decodifica barras
 }
 
-void verbarra_pbm(int imagem[HEIGHT][WIDTH], int *x, int *y, int *larg, int *alt){
-    //dectecta região da barra
-    //procurar no doc tamanhos das dimensoes aceitas 
+int checasoma(int decode_d[]){
+    int soma=0;
+    for(int i=0;i<8;i++){
+        soma+=decode_d[i] * (i%2==0? 3 : 1); //considera o peso da soma
+    }
+    //digito verificador
+    int verifica=(ceil(soma/10))*10-soma;
+    
+    //talvez seja útil
+    //verifica = (10 - (sum % 10)) % 10;
+    //
+    return verifica;
 }
+
+//os dois acima ficam em um arquivo h
