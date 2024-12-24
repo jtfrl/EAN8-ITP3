bd6d0a44cf234b80ee9cbdce7d09fff8272bf78a
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2024-12-24 15:05:35 -0300

    pbm.h
    Código para poder ler imagens em .pbm (em dev).

---
+++ pbm.h
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdbool.h> //código a ser feito ainda
+#define WIDTH 100 //colocar esse números no arquivo h
+#define HEIGHT 100
+
+const int start_end_pattern[] = {1, 0, 1};
+const int f_end_pattern[] = {1, 0, 1};
+const int c_end_pattern[] = {0, 1, 0, 1, 0};
+const int left_digit_patterns[10][7] = {
+    {0, 0, 0, 1, 1, 0, 1},
+    {0, 0, 1, 1, 0, 0, 1},
+    {0, 0, 1, 0, 0, 1, 1},
+    {0, 1, 1, 1, 1, 0, 1},
+    {0, 1, 0, 0, 0, 1, 1},
+    {0, 1, 1, 0, 0, 0, 1},
+    {0, 1, 0, 1, 1, 1, 1},
+    {0, 1, 1, 1, 0, 1, 1},
+    {0, 1, 1, 0, 1, 1, 1},
+    {0, 0, 0, 1, 0, 1, 1}
+};
+
+
+const int right_digit_patterns[10][7] = {
+   {1, 1, 1, 0, 0, 1, 0}
+   {1, 1, 0, 0, 1, 1, 0}
+   {1, 1, 0, 1, 1, 0, 0}
+   {1, 0, 0, 0, 0, 1, 0}
+   {1, 0, 1, 1, 1, 0, 0}
+   {1, 0, 0, 1, 1, 1, 0}
+   {1, 0, 1, 0, 0, 0, 0}
+   {1, 0, 0, 0, 1, 0, 0}
+   {1, 0, 0, 1, 0, 0, 0}
+   {1, 1, 1, 0, 1, 0, 0}
+};
+//funções pra deixar em arquivo h
+void carrega_pbm(const char *filename, int image[HEIGHT][WIDTH]){
+    //carregamento de imagem aqui
+}
+
+void verbarra_pbm(int imagem[HEIGHT][WIDTH], int *x, int *y, int *larg, int *alt){
+    //dectecta região da barra
+    //procurar no doc tamanhos das dimensoes aceitas 
+}
