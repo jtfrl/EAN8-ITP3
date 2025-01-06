5118ad7ddee2c1e2ccca26bbda68f6ba6c92e7fb
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-01-05 22:20:24 -0300

    Create pImg.c
    Arquivo fonte auxiliar do io_pbm.h

---
+++ pImg.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#define WIDTH 209
+#define HEIGHT 58   
+#define START_STOP_MOD_TAM 3 //marcador de final e início
+#define SAFETY_OFFSET 2
+
+typedef struct{
+    int width, height;
+    unsigned char* pImg;
+    unsigned char* pMeio;
+    unsigned char* barra_larg;
+}imgInfo;
+
+const unsigned char* passaMargem(const unsigned char* buf);
+unsigned short largBarra(const unsigned char* buf);
+void carrega_pbm(const char *filename, int image[HEIGHT][WIDTH]);
+void verbarra_pbm(int imagem[HEIGHT][WIDTH], int *x, int *y, int *larg, int *alt);
+
