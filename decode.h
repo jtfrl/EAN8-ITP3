03ef868aa410e910445387bd87fe3728eb282f30
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-01-08 00:33:07 -0300

    Update decode.h
    Corrigindo função de status

--- decode.h
+++ decode.h
@@ -1,100 +1,138 @@
-#ifndef DECODE_H
-#define DECODE_H
-
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 #include <errno.h>
-#include <math.h>
-#include "io_pbm.h" 
-
-const int start_end_pattern[] = {1, 0, 1};
-const int f_end_pattern[] = {1, 0, 1};
-const int c_end_pattern[] = {0, 1, 0, 1, 0};
-const int left_digit_patterns[10][7] = {
-    {0, 0, 0, 1, 1, 0, 1},
-    {0, 0, 1, 1, 0, 0, 1},
-    {0, 0, 1, 0, 0, 1, 1},
-    {0, 1, 1, 1, 1, 0, 1},
-    {0, 1, 0, 0, 0, 1, 1},
-    {0, 1, 1, 0, 0, 0, 1},
-    {0, 1, 0, 1, 1, 1, 1},
-    {0, 1, 1, 1, 0, 1, 1},
-    {0, 1, 1, 0, 1, 1, 1},
-    {0, 0, 0, 1, 0, 1, 1}
-};
+#include <string.h>
+#include "pImg.h" //o programa toma as declarações implícitas de io_pbm.h
+#include "decode.h"
 
 
-const int right_digit_patterns[10][7] = {
-   {1, 1, 1, 0, 0, 1, 0},
-   {1, 1, 0, 0, 1, 1, 0},
-   {1, 1, 0, 1, 1, 0, 0},
-   {1, 0, 0, 0, 0, 1, 0},
-   {1, 0, 1, 1, 1, 0, 0},
-   {1, 0, 0, 1, 1, 1, 0},
-   {1, 0, 1, 0, 0, 0, 0},
-   {1, 0, 0, 0, 1, 0, 0},
-   {1, 0, 0, 1, 0, 0, 0},
-   {1, 1, 1, 0, 1, 0, 0}
-};
+#define WIDTH 209
+#define HEIGHT 58
 
 
-void decode_ean8(int bin_representa[], int decode_d[], int *status){
-    //decodifica barras
-    //busca pelos dígitos iniciais e os dígitos finais
+int cabeca_pbm(FILE *file, int *width, int *height){
+    char format[3];
+    if(fscanf(file, "%2s", format)!=1 || strcmp(format, "P1")!=0){
+        fprintf(stderr, "ERRO: não é um arquivo PBM válido\n");
+        return -1; 
+    }//leitura da formatação: checa se o formato P1 esta indicado
 
- 
-    for (int i=0;i<4;i++){
-        //decodifica os numeros da esquerda
-        for(int j=0;j<10;j++){
-            int corretoEsq=1;
-            for (int k=0; k<7; k++){
-                if(bin_representa[i*7+k]!=left_digit_patterns[j][k]){
-                    corretoEsq=0;
-                    break;
-                }
-            }
-            if(corretoEsq){
-                decode_d[i]=j;
-                break;
+
+    if(fscanf(file, "%d %d", width, height)!=2){
+        fprintf(stderr, "ERRO: falha na leitura das dimensões da imagem\n");
+        return -1;
+    }//leitura de dimensões
+    
+    return 0;
+}
+
+
+unsigned char** dados_pbm(FILE *file, int width, int height){
+    unsigned char **image = (unsigned char **)malloc(height *sizeof(unsigned char *));
+    //cria espaço na memória para altura da imagem fornecida 
+    if(image==NULL){
+        fprintf(stderr, "ERRO: falha na alocação de memória\n");
+        return NULL;
+    }
+
+    for (int i=0; i<height;i++){
+        image[i]=(unsigned char *)malloc(width * sizeof(unsigned char));
+        if (image[i]==NULL){
+            fprintf(stderr, "ERRO: falha de alocação da memória nas linhas da imagem\n");
+            return NULL;
+        }
+    }
+
+    for (int i=0; i<height;i++){
+        for(int j=0;j<width;j++){
+            if((fscanf(file,"%hhu", &image[i][j])!=1)){ //alterar comparação de ponteiro
+                fprintf(stderr,"ERRO: falha em ler dados de imagem\n");
+                return NULL;
             }
         }
     }
 
-     for (int i=4;i<8;i++){
-        //decodifica os numeros da direita
-        for(int j=0;j<10;j++){
-            int corretoDir=1; //verificador de correção para direita
-            for (int k=0; k<7; k++){
-                if(bin_representa[(i*7)+k]!=right_digit_patterns[j][k]){
-                    corretoDir=0;
-                    break;
-                }
-            }
-            if(corretoDir){
-                decode_d[i]=j;
-                break;
-            }
+
+    return image;
+
+}
+
+/*função que vai liberar a alocação de memória
+void libera_pbm(unsigned char **image, int height){
+    for (int i=0;i<height;i++){
+        free(image[i]);
+    }
+    free(image);
+}
+*/
+
+void libera_pbm(int dadosImg[HEIGHT][WIDTH], int height);
+//declaração implícita
+
+int main(int argc, char*argv[]){ 
+
+    int image[HEIGHT][WIDTH] = {0}; 
+    int status;
+    int x, y, larg, alt;
+    int bin_representa[56]={0}, decode_d[8]={0};
+    //IMPORTANTE inlcuir variáveis usadas em carregapbm
+    
+    //main que vai receber o que o usuário irá fornecer
+    if(argc!=2){
+        fprintf(stderr, "Usage: %s <pbm_file>\n", argv[0]);
+        return 1;
+    }
+    //arquivo é aberto
+    FILE *arquivo=fopen(argv[1], "r");
+        //ocorre a leitura
+    if(arquivo==NULL){
+        fprintf(stderr, "ERRO: falha em abrir o arquivo %s\n", argv[1]);
+        return 1;
+    }
+    
+
+    int width, height;
+    /* TESTE: verificando se o trecho está causando redefinição
+    if(cabeca_pbm(arquivo, &width, &height)!=0){
+        fclose(arquivo);
+        return 1;
+    }
+    */
+    fclose(arquivo);
+
+
+    carrega_pbm(argv[1], image);
+    //argv[1] guarda o nome do arquivo em .pbm
+    verbarra_pbm(image, &x, &y, &larg, &alt);
+    decode_ean8(bin_representa, decode_d, status);
+    checasoma(decode_d);
+
+
+    
+    int checaSoma=checasoma(decode_d);
+
+
+
+    if(status!=0 && checaSoma!=0){
+        printf("Código EAN-8\n");
+        for (int k=0; k<8;k++){
+            printf("%d ", decode_d[k]);
+        }
+        printf("\n");
+    }
+
+    int dadosImg[HEIGHT][WIDTH];
+    //copia os elementos da imagem para a variável
+    for(int i=0;i<HEIGHT;i++){
+        for (int j=0;j<WIDTH;j++){
+            dadosImg[i][j]=image[i][j];
         }
     }
+    
+    libera_pbm(dadosImg,height);
 
-    if(corretoEsq==0 && corretoDir==0){
-        *status=0;
-    }else{
-        *status=1;//indica sucesso
-    }
-}
-
-int checasoma(int decode_d[]){
-    int soma=0;
-    for(int i=0;i<8;i++){
-        int pesosEAN=(i%2==0? 3 : 1); 
-        soma+=(decode_d[i])*(pesosEAN); //considera o peso da soma
-        //se par: 3; se ímpar: 1
-    }
-    //digito verificador
-    int verifica=(int)(ceil(soma/10))*10-soma;
-    return verifica;
+    return 0;
 }
 
 
-#endif //previne redefinição com DECODE_H
