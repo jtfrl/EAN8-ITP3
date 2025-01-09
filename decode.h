02b3a53340c61996facf7deba16dd3a9b15c2d1b
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-01-09 01:38:00 +0000

    Mudanças para concluir a alocação de memória

--- decode.h
+++ decode.h
@@ -1,8 +1,11 @@
+#ifndef DECODE_H
+#define DECODE_H
+
 #include <stdio.h>
 #include <stdbool.h>
-#include "ean8.h" //código a ser feito ainda
-#define WIDTH 100 //colocar esse números no arquivo h
-#define HEIGHT 100
+#include <errno.h>
+#include <math.h>
+#include "io_pbm.h" 
 
 const int start_end_pattern[] = {1, 0, 1};
 const int f_end_pattern[] = {1, 0, 1};
@@ -22,34 +25,79 @@ const int left_digit_patterns[10][7] = {
 
 
 const int right_digit_patterns[10][7] = {
-   {1, 1, 1, 0, 0, 1, 0}
-   {1, 1, 0, 0, 1, 1, 0}
-   {1, 1, 0, 1, 1, 0, 0}
-   {1, 0, 0, 0, 0, 1, 0}
-   {1, 0, 1, 1, 1, 0, 0}
-   {1, 0, 0, 1, 1, 1, 0}
-   {1, 0, 1, 0, 0, 0, 0}
-   {1, 0, 0, 0, 1, 0, 0}
-   {1, 0, 0, 1, 0, 0, 0}
+   {1, 1, 1, 0, 0, 1, 0},
+   {1, 1, 0, 0, 1, 1, 0},
+   {1, 1, 0, 1, 1, 0, 0},
+   {1, 0, 0, 0, 0, 1, 0},
+   {1, 0, 1, 1, 1, 0, 0},
+   {1, 0, 0, 1, 1, 1, 0},
+   {1, 0, 1, 0, 0, 0, 0},
+   {1, 0, 0, 0, 1, 0, 0},
+   {1, 0, 0, 1, 0, 0, 0},
    {1, 1, 1, 0, 1, 0, 0}
 };
 
-void decode_ean8(int bin_representa[], int decode_d[]){
+
+void decode_ean8(int bin_representa[], int decode_d[], int *status){
     //decodifica barras
+    //busca pelos dígitos iniciais e os dígitos finais
+
+ 
+    for (int i=0;i<4;i++){
+        //decodifica os numeros da esquerda
+        for(int j=0;j<10;j++){
+            int corretoEsq=1;
+            for (int k=0; k<7; k++){
+                if(bin_representa[i*7+k]!=left_digit_patterns[j][k]){
+                    corretoEsq=0;
+                    break;
+                }
+            }
+            if(corretoEsq){
+                decode_d[i]=j;
+                break;
+            }
+        }
+    }
+    int val_corretoEsq=corretoEsq;
+
+     for (int i=4;i<8;i++){
+        //decodifica os numeros da direita
+        for(int j=0;j<10;j++){
+            int corretoDir=1; //verificador de correção para direita
+            for (int k=0; k<7; k++){
+                if(bin_representa[(i*7)+k]!=right_digit_patterns[j][k]){
+                    corretoDir=0;
+                    break;
+                }
+            }
+            if(corretoDir){
+                decode_d[i]=j;
+                break;
+            }
+        }
+    }
+    int val_corretoDir=corretoDir;
+
+
+    if(val_corretoEsq==0 && val_corretoDir==0){
+        *status=0;
+    }else{
+        *status=1;//indica sucesso
+    }
 }
 
 int checasoma(int decode_d[]){
     int soma=0;
     for(int i=0;i<8;i++){
-        soma+=decode_d[i] * (i%2==0? 3 : 1); //considera o peso da soma
+        int pesosEAN=(i%2==0? 3 : 1); 
+        soma+=(decode_d[i])*(pesosEAN); //considera o peso da soma
+        //se par: 3; se ímpar: 1
     }
     //digito verificador
-    int verifica=(ceil(soma/10))*10-soma;
-    
-    //talvez seja útil
-    //verifica = (10 - (sum % 10)) % 10;
-    //
+    int verifica=(int)(ceil(soma/10))*10-soma;
     return verifica;
 }
 
-//os dois acima ficam em um arquivo h
+
+#endif //previne redefinição com DECODE_H
---
+++ io_pbm.h
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "pImg.h"
+
+const unsigned char* passaMargem(const unsigned char* buf){
+    while (*buf==1){
+        buf+=1;
+    }
+    return buf;
+}
+
+unsigned short largBarra(const unsigned char* buf){
+    unsigned short larg_Barra=0;
+    while (*buf==0){
+        buf+=1;
+        larg_Barra+=1;
+    }
+    buf-=larg_Barra;
+    return larg_Barra;
+}
+
+//IMPORTANTE: a parte acima acho que vai ser válida para decode.h
+
+void carrega_pbm(const char *filename, int image[HEIGHT][WIDTH]){
+    FILE *arquivo=fopen(filename, "r");
+    //aqui começa a leitura de fato e o carregamento em si
+    if(arquivo==NULL){
+        perror("Erro ao abrir arquivo");
+        exit(EXIT_FAILURE);
+    }
+    //podemos partir do ponto em que a checagem de formato já foi feita
+    char format[3];
+    int width, height;
+    fscanf(arquivo, "%2s, %d, %d", format, &width, &height);
+
+    //leitura dos dados a serem processados
+    for(int y=0; y<HEIGHT; y++){
+        for(int x=0; x<WIDTH; x++){
+            char c;
+            do{
+                c=fgetc(arquivo);
+            }while(c!='0' && c!='1' && c!=EOF);
+            if(c==EOF){
+                fprintf(stderr, "Erro inesperado de fim de arquivo\n");
+                fclose(arquivo);
+                exit(EXIT_FAILURE);
+            }
+
+            image[y][x]=(c=='1') ? 1 : 0;
+
+            //avalia cada dado em imagem, sendo um ou zero 
+        }
+    }
+
+    fclose(arquivo);
+
+}
+
+void verbarra_pbm(int imagem[HEIGHT][WIDTH], int *x, int *y, int *larg, int *alt){
+        //converte a imagem de 2D para 1D
+        unsigned char* pImg = (unsigned char*)malloc(HEIGHT * WIDTH *sizeof(unsigned char));
+        for(int y=0;y<HEIGHT;y++){
+            for(int x=0;x<WIDTH;x++){
+                pImg[y*WIDTH+x]=(unsigned char)imagem[y][x];
+            }
+        }
+
+        //move para o meio
+
+        int yMeio=HEIGHT/2;
+        unsigned char* pMeio=pImg+yMeio*WIDTH;
+
+        //Passa pela margem, para pegar a largura das barras
+        const unsigned char* buf=passaMargem(pMeio);
+        unsigned short larg_Barra=largBarra(buf);
+
+        //Calcula as coodernadas e as dimensões da região da barra
+        *x=(buf-pMeio)*larg_Barra;
+        *y=yMeio;
+        *larg=WIDTH; //assume que o código de barras abarca largura pre definida
+        *alt=larg_Barra*(START_STOP_MOD_TAM*2+8*7+5);//total das áreas em codificação
+
+        free(pImg);
+
+}
