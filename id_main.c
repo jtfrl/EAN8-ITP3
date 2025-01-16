92650dca8f3d2eef1ac9e229e2c0cfeea1f2d0db
Jefferson Tafarel <46331858+jtfrl@users.noreply.github.com>
2025-01-16 00:11:16 -0300

    Update id_main.c
    Alterações para exibição do código EAN-8; criação da variável image_novo

--- id_main.c
+++ id_main.c
@@ -11,15 +11,15 @@
 #define HEIGHT 58
 
 
-int cabeca_pbm(FILE *file, int *width, int *height){
+int cabeca_pbm(FILE *arquivo, int *width, int *height){
     char format[3];
-    if(fscanf(file, "%2s", format)!=1 || strcmp(format, "P1")!=0){
+    if(fscanf(arquivo, "%2s", format)!=1 || strcmp(format, "P1")!=0){
         fprintf(stderr, "ERRO: não é um arquivo PBM válido\n");
         return -1; 
     }//leitura da formatação: checa se o formato P1 esta indicado
 
 
-    if(fscanf(file, "%d %d", width, height)!=2){
+    if(fscanf(arquivo, "%d %d", width, height)!=2){
         fprintf(stderr, "ERRO: falha na leitura das dimensões da imagem\n");
         return -1;
     }//leitura de dimensões
@@ -28,7 +28,7 @@ int cabeca_pbm(FILE *file, int *width, int *height){
 }
 
 
-unsigned char** dados_pbm(FILE *file, int width, int height){
+unsigned char** dados_pbm(FILE *arquivo, int width, int height){
     unsigned char **image = (unsigned char **)malloc(height *sizeof(unsigned char *));
     //cria espaço na memória para altura da imagem fornecida 
     if(image==NULL){
@@ -46,7 +46,7 @@ unsigned char** dados_pbm(FILE *file, int width, int height){
 
     for (int i=0; i<height;i++){
         for(int j=0;j<width;j++){
-            if((fscanf(file,"%hhu", &image[i][j])!=1)){ //alterar comparação de ponteiro
+            if((fscanf(arquivo,"%hhu", &image[i][j])!=1)){ //alterar comparação de ponteiro
                 fprintf(stderr,"ERRO: falha em ler dados de imagem\n");
                 return NULL;
             }
@@ -75,12 +75,12 @@ int main(int argc, char*argv[]){
         image_dnm[i]=(int *)malloc(WIDTH *sizeof(int));
     }
 
-    carrega_pbm(argv[1], image_d);
-    
-    int image[HEIGHT][WIDTH] = {0};
+    //int image[HEIGHT][WIDTH] = {0}; 
+
     int status;
     int x, y, larg, alt;
     int width, height;
+    unsigned char **image_novo; //variável para receber dados do pbm
     int bin_representa[56]={0}, decode_d[8]={0};
     
     if(argc!=2){
@@ -95,19 +95,26 @@ int main(int argc, char*argv[]){
         return 1;
     }
     
+    
+    if(cabeca_pbm(arquivo, &width, &height)!=0){
+        fclose(arquivo);
+        return 1;
+    }
+    
+    image_novo=dados_pbm(arquivo, width, height);
+    if(image_novo==NULL){
+        fclose(arquivo);
+        return 1;
+    }
 
-    fclose(arquivo);
-
+    fclose(arquivo); 
     //argv[1] guarda o nome do arquivo em .pbm
     verbarra_pbm(image, &x, &y, &larg, &alt);
     decode_ean8(bin_representa, decode_d, &status);
-    checasoma(decode_d);
-
+    //checasoma(decode_d);
 
     
     int checaSoma=checasoma(decode_d);
-
-
 
     if(status!=0 && checaSoma!=0){
         printf("Código EAN-8\n");
@@ -117,6 +124,7 @@ int main(int argc, char*argv[]){
         printf("\n");
     }
 
+    /*
     int dadosImg[HEIGHT][WIDTH];
     //copia os elementos da imagem para a variável
     for(int i=0;i<HEIGHT;i++){
@@ -124,10 +132,10 @@ int main(int argc, char*argv[]){
             dadosImg[i][j]=image[i][j];
         }
     }
+    */
     
     libera_pbm(image_dnm,height);
     //libera para image
 
     return 0;
 }
-
